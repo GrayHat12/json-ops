9fd158662e095c9f599a580b4a12b14bb480e9c9
GrayHat <grayhathacks10@gmail.com>
2023-09-23 16:49:36 +0530

    bugfixes in vite pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, searchForWorkspaceRoot } from 'vite'
+import { defineConfig, searchForWorkspaceRoot} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
@@ -7,25 +7,26 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      mode: "development",
+      mode: "production",
       minify: true,
       manifest: {
+        start_url: "/json-ops/",
         orientation: "portrait",
         name: "Json Comparison",
         background_color: "#448aff",
         theme_color: "#0072f4",
         display: "standalone",
         icons: [
-          { src: "/icons/maskable_icon_x48.png", type: "image/png", sizes: "48x48", purpose: "maskable" },
-          { src: "/icons/maskable_icon_x72.png", type: "image/png", sizes: "72x72", purpose: "maskable" },
-          { src: "/icons/maskable_icon_x96.png", type: "image/png", sizes: "96x96", purpose: "maskable" },
-          { src: "/icons/maskable_icon_x128.png", type: "image/png", sizes: "128x128", purpose: "maskable" },
-          { src: "/icons/maskable_icon_x192.png", type: "image/png", sizes: "192x192", purpose: "maskable" },
-          { src: "/icons/maskable_icon_x384.png", type: "image/png", sizes: "384x384", purpose: "maskable" },
-          { src: "/icons/maskable_icon_x512.png", type: "image/png", sizes: "512x512", purpose: "maskable" },
-          { src: "/icons/maskable_icon.png", type: "image/png", sizes: "1024x1024", purpose: "maskable" },
-          { src: "/icons/maskable_icon.png", type: "image/png", sizes: "1024x1024" },
-          { src: "/icon.svg", type: "image/png", sizes: "320x320" },
+          { src: "/json-ops/icons/maskable_icon_x48.png", type: "image/png", sizes: "48x48", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon_x72.png", type: "image/png", sizes: "72x72", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon_x96.png", type: "image/png", sizes: "96x96", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon_x128.png", type: "image/png", sizes: "128x128", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon_x192.png", type: "image/png", sizes: "192x192", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon_x384.png", type: "image/png", sizes: "384x384", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon_x512.png", type: "image/png", sizes: "512x512", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon.png", type: "image/png", sizes: "1024x1024", purpose: "maskable" },
+          { src: "/json-ops/icons/maskable_icon.png", type: "image/png", sizes: "1024x1024" },
+          { src: "/json-ops/icon.svg", type: "image/png", sizes: "320x320" },
         ]
       },
       workbox: {
@@ -33,7 +34,7 @@ export default defineConfig({
       },
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        enabled: false
       }
     })
   ],
