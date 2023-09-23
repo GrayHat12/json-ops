8997cff703aa1e014ee2f3affc149aaac75833d6
GrayHat <grayhathacks10@gmail.com>
2023-09-23 16:48:05 +0530

    Updates

--- sw.js
+++ sw.js
@@ -135,35 +135,8 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "url": "registerSW.js",
     "revision": "48bb6f73a84866139b97ebd2a493d967"
   }, {
-    "url": "icons/maskable_icon_x48.png",
-    "revision": "196364073593bb4c1753ded2796d2b75"
-  }, {
-    "url": "icons/maskable_icon_x72.png",
-    "revision": "a2c67e0309b8ce89c0456507e7c864e9"
-  }, {
-    "url": "icons/maskable_icon_x96.png",
-    "revision": "600952d3ff7912aedf7f944bda55906f"
-  }, {
-    "url": "icons/maskable_icon_x128.png",
-    "revision": "f2a55a2cbeb1119379649aa515cc9c2e"
-  }, {
-    "url": "icons/maskable_icon_x192.png",
-    "revision": "b9ac9b05e54d577f24677180e81dc479"
-  }, {
-    "url": "icons/maskable_icon_x384.png",
-    "revision": "34b31afd667a76b3e8bde9d65fe041a7"
-  }, {
-    "url": "icons/maskable_icon_x512.png",
-    "revision": "312e9f40ee4fb4844d0344a68df29aba"
-  }, {
-    "url": "icons/maskable_icon.png",
-    "revision": "581496adeca9aa460c920dd287b5532c"
-  }, {
-    "url": "icon.svg",
-    "revision": "ef773a3066a9edbdf374b026142ccb87"
-  }, {
     "url": "manifest.webmanifest",
-    "revision": "b984d8d5707deb31f9294d5ea4905483"
+    "revision": "383752943a17d91e3b1c9209b5317086"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
